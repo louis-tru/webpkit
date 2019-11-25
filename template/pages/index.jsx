443bf09759ef750f39d7781a0d398936998c24f0
chuxuewen <chuxuewen@hard-chain.cn>
2019-11-25 16:45:19 +0800

    up template

---
+++ template/pages/index.jsx
@@ -0,0 +1,74 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2019, hardchain
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of hardchain nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL hardchain BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+import utils from 'nxkit';
+import req from 'nxkit/request';
+import path from 'nxkit/path';
+import {Page,React} from 'cport-h5';
+import './index.css';
+
+const LINES = 100;
+
+export default class A extends Page {
+	
+	state = { logs : [], color: path.getParam('color') || '0f0' };
+
+	async showLog() {
+		var {data:{text}} = await req.get(`./static/${path.getParam('log')||'log'}.txt`);
+		var logsAll = text.split(/\n/);
+		var index = 0, len = logsAll.length;
+		var logs = [];
+		while (true) {
+			logs.push(logsAll[index]);
+			if (logs.length > LINES) {
+				logs.shift();
+			}
+			this.setState({ logs });
+			await utils.sleep(utils.random(50, 1000));
+			index = (index + 1) % len;
+		}
+	}
+
+	onLoad() {
+		this.showLog();
+	}
+
+	render() {
+		return (
+			<div className="index">
+				<div className="con">
+					{this.state.logs.map((e,i)=>
+						<div className="log" style={{color:'#'+this.state.color}} key={i}>{e}</div>
+					)}
+				</div>
+			</div>
+		);
+	}
+}
